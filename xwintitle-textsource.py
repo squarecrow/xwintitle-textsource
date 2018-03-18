4a0291d94065e67eea6d6dd0c32d8779a44c05ef
squarecrow <k1@1nce.org>
2018-03-18 14:51:14 +0100

    Update xwintitle-textsource.py

--- xwintitle-textsource.py
+++ xwintitle-textsource.py
@@ -3,14 +3,10 @@ try: # for standalone testing
 except: pass
 import subprocess
 
-shell = "wmctrl -l"
-
-title_ends=[]
+mediaplayers=[]
 
 def cut_end(arg):
-  #with open('title-suffixes.txt', 'r') as f:
-#    title_ends = [i.rstrip('\r\n') for i in f.readlines()]
-  for line in title_ends:
+  for line in mediaplayers:
     end = ' - ' + line
     if end in arg:
       arg = arg.split( end )[0]
@@ -18,6 +14,7 @@ def cut_end(arg):
 
 def get_windows():
   """ Get and return all window objects. """
+  shell = "wmctrl -l"
   windows = subprocess.check_output(["/bin/bash", "-c", shell])
   windows = windows.decode("utf-8").splitlines()
   for index,item in enumerate(windows):
@@ -28,40 +25,34 @@ def get_windows():
     windows[index] = item
   return windows
 
-target_id = ""
-target_title = ""
-refresh = 700
-source_name = ""
-title_ends_file = ""
+def obs_warning(string):
+  """ Use OBS's warning log. """
+  return obs.script_log(obs.LOG_WARNING, string)
 
-def modify(arg):
-  return arg
+target_id = target_title = source_name = mp_file = ""
+refresh = 700
+
+def force_update(props, prop):
+  update_text()
 
 def update_text():
-  global refresh
   global source_name
   global target_id
   global target_title
-  source = obs.obs_get_source_by_name(source_name)
   
+  source = obs.obs_get_source_by_name(source_name)
   for i in get_windows():
     if target_id in i[0]:
       target_title = i[3]
   target_title = modify(target_title)
+  settings = obs.obs_data_create()
+  obs.obs_data_set_string( settings, "text", target_title)
   if source is not None:
-    settings = obs.obs_data_create()
-    obs.obs_data_set_string(settings, "text", target_title)
-    obs.obs_source_update(source, settings)
-    obs.obs_data_release(settings)
+    obs.obs_source_update(   source  , settings)
+    obs.obs_data_release(    settings)
   else:
-      obs.script_log(obs.LOG_WARNING, "No text source found!")
+      obs_warning("Choose a text source!")
       obs.remove_current_callback()
-
-def force_update(props, prop):
-  global title_ends
-  with open(title_ends_file, 'r') as f:
-   title_ends = [i.rstrip('\r\n') for i in f.readlines()]
-  update_text()
 
 def script_description():
   return ("Write a window ID's title to a text source.")
@@ -74,13 +65,17 @@ def script_update(settings):
   global target_id
   global refresh
   global source_name
-  global title_ends
-  global title_ends_file
+  global mediaplayers
+  global mp_file
   target_id   = obs.obs_data_get_string(settings, "windowlist")
-  title_ends_file  = obs.obs_data_get_string(settings, "title_end")
+  mp_file = obs.obs_data_get_string(settings, "title_end")
   refresh     = obs.obs_data_get_int(   settings, "update")
   source_name = obs.obs_data_get_string(settings, "source")
-  update_text()
+  try:
+    with open(mp_file, 'r') as f:
+      mediaplayers = [l.strip('\n') for l in f.readlines()]
+  except:
+      obs_warning("Open a list file!")
   obs.timer_remove(update_text)
   obs.timer_add(update_text, refresh)
 
@@ -126,5 +121,5 @@ def script_properties():
     obs.source_list_release(sources)
   
   obs.obs_properties_add_button(props,
-  "button", "Update", force_update)
+  "btn_update", "Update", force_update)
   return props
